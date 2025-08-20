1e4f6120b6a52649ee0e61d514fd9da575605885
Marco Avila <marcoavila00@gmail.com>
2025-08-20 19:31:12 +0200

    Rename react-frontend to frontend for clarity

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header style={{ background: '#282c34', padding: '1rem', color: 'white' }}>
+    <h1>Welcome to My React App</h1>
+  </header>
+);
+
+export default Header;
