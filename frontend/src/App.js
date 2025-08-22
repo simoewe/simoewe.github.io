4f05072bd5893dab290cc8fc2779237eddb6beea
simoewe <derdersimon@gmx.net>
2025-08-22 10:50:03 +0200

    Restore Alten Build von Marco

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './components/Header';
+import TextAnalyzer from './components/TextAnalyzer';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+  <TextAnalyzer />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <small>© {new Date().getFullYear()} Web Analyzer</small>
-    </footer>
-  )
-}
