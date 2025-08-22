bd13fba4b4157104222779f0533df188c389ee3c
Marco Avila <marcoavila00@gmail.com>
2025-08-22 16:37:21 +0200

    Pleaseeeeeeee!

--- Backend/app.py
+++ Backend/app.py
@@ -16,7 +16,7 @@ import re
 
 # Initialize Flask
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins for now; restrict in production
+CORS(app, resources={r"/*": {"origins": "https://simoewe-github-io-1-cdi0.onrender.com"}}) # Allow specific origin;
 nlp = spacy.load('en_core_web_sm')
 
 # Configure logging
